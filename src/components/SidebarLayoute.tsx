b20c604d2d4fb011a0442a766099c2239ca08df1
Fábio Brasileiro <brasileirowinner@gmail.com>
2025-08-28 21:51:55 -0300

    feat(sidebarLayout): Implementar sidebarLayout

---
+++ src/components/SidebarLayoute.tsx
@@ -0,0 +1,55 @@
+import React, { ReactNode } from 'react'
+import Sidebar from './sidebar'
+import { CreditCard, LinkIcon, ShoppingCart, Target, User, Users } from 'lucide-react';
+
+const sidebarLayout = ({ children }: { children: ReactNode }) => {
+   const menuItems = [
+    {
+      id: 'minhas-vendas',
+      label: 'Minhas Vendas',
+      icon: <ShoppingCart className="h-4 w-4" />,
+      href: '/conta'
+    },
+    {
+      id: 'conta-de-saque',
+      label: 'Conta de Saque Iugu',
+      icon: <CreditCard className="h-4 w-4" />,
+      href: '/conta/saque'
+    },
+    {
+      id: 'perfil',
+      label: 'Perfil',
+      icon: <User className="h-4 w-4" />,
+      href: '/conta/perfil'
+    },
+    {
+      id: 'hotlinks',
+      label: 'Hotlinks',
+      icon: <LinkIcon className="h-4 w-4" />,
+      href: '/conta/hotlink'
+    },
+  
+    {
+      id: 'meus-afiliados',
+      label: 'Meus Afiliados',
+      icon: <Users className="h-4 w-4" />,
+      href: '/conta/afiliado'
+    }
+  ];
+
+    return (
+        <>
+            <div className="flex h-screen">
+                <div className="h-screen">
+                    <Sidebar items={menuItems} title='Minha Conta' />
+                </div>
+
+                <div className='w-full'>
+                    {children}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default sidebarLayout
