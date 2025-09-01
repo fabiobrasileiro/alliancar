d2b2da2db1befae83bde248e79240fe0c1900169
Fábio Brasileiro <brasileirowinner@gmail.com>
2025-09-01 19:43:10 -0300

    feat(userProvider): Adicionar uma forma de pegar as informações do usuario assim que ele logar na aplicação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavbarWrapper from "@/components/navbar-wrapper";
+import { UserProvider } from "@/context/UserContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavbarWrapper>{children}</NavbarWrapper>
+
+        <UserProvider>
+          <NavbarWrapper>{children}</NavbarWrapper>
+        </UserProvider>
+
+
       </body>
     </html>
   );
